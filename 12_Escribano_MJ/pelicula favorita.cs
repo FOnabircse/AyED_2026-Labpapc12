3202db906f8b4215a71b03be448f8da62ed9351f
FOnabircse <escribanofabricio1@gmail.com>
2026-03-26 16:13:36 -0300

    Add files via upload

---
+++ 12_Escribano_MJ/pelicula favorita.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Cual es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+
+            Console.WriteLine("Hola amigo " + nombre + " con el respeto que se merece " + pelicula + " es una mierda");
+        }
+    }
+}
