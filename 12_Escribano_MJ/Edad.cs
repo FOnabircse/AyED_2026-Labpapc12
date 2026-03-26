b2a6099d2e76a3850e15800106f1afabbcaabeea
FOnabircse <escribanofabricio1@gmail.com>
2026-03-26 16:54:32 -0300

    Add files via upload

---
+++ 12_Escribano_MJ/Edad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int adn;
+            int edad;
+            Console.Write("¿Cual es tu año de nacimiento?: ");
+            adn = Convert.ToInt32(Console.ReadLine());
+
+            edad = (2026 - adn);
+
+            Console.WriteLine("Si naciste en el año " + adn + " tu edad al comenzar el año es de " + edad);
+        }
+    }
+}
