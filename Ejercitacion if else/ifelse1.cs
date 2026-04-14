f29103567ccaeb3616b274e3dd03c7d68cf9bc82
FOnabircse <escribanofabricio1@gmail.com>
2026-04-14 15:19:52 -0300

    Add files via upload

---
+++ Ejercitacion if else/ifelse1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double peso;
+            Double diferencia;
+
+            Console.Write("Ingrese su peso ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            if (peso <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado.Buen viaje, Mando.");
+            }
+
+            else 
+            {
+                diferencia = (peso - 5000);
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar " + diferencia + " kg para despegar ");
+            }
+        }
+    }
+}
