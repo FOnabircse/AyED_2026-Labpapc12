695e7c6faa429b8e1c44e4a726684d675d0ac48c
FOnabircse <escribanofabricio1@gmail.com>
2026-05-07 15:57:39 -0300

    Add files via upload

---
+++ switchtp/temperatura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double acumulador = 0;
+            double temperatura;
+            double contador = 0;
+            for (int dia = 1; dia <= 7; dia++)
+            {
+                Console.Write("Ingrese la temperatura del dia " + dia +": ");
+                temperatura = double.Parse(Console.ReadLine());
+                acumulador = temperatura + acumulador;
+
+                if (temperatura < 0)
+                {
+                    contador ++;
+                }
+                if (dia == 7)
+                {
+                    Console.WriteLine("Hubieron " + contador +" dias abajo de 0° y la suma de la temperatura de todos los dias fue "+ acumulador +".");
+                }
+            } 
+        }
+    }
+}
