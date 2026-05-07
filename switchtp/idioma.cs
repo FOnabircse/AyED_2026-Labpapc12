ae1f20d63da4bd71f9bf229fca7c5cce6521f6ad
FOnabircse <escribanofabricio1@gmail.com>
2026-05-07 15:17:52 -0300

    Add files via upload

---
+++ switchtp/idioma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eliga el idioma");
+            Console.WriteLine("1: Ingles");
+            Console.WriteLine("2: Francés");
+            Console.WriteLine("3: Alemán");
+            Console.Write("Indique la opción que quiera: ");
+
+            String opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1": Console.WriteLine("Hello"); break;
+                case "2": Console.WriteLine("Salut"); break;
+                case "3": Console.WriteLine("Hallo"); break;
+                default: Console.WriteLine("Idioma no encontrado"); break;
+            }
+        }
+    }
+}
---
+++ switchtp/sandwich.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique la opción que quiera:");
+            Console.WriteLine("Pizza (P)");
+            Console.WriteLine("Sandwich (S)");
+            Console.WriteLine("Hamburguesa (H)");
+            Console.Write("Eliga lo que quiera comer: ");
+
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case "P": Console.WriteLine("Pizza $12000"); break;
+                case "S": Console.WriteLine("Sandwich $7000"); break;
+                case "H": Console.WriteLine("Hamburguesa $8000"); break;
+                default: Console.WriteLine("Producto no encontrado"); break;
+            }
+
+        }
+    }
+}
