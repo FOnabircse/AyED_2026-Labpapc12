76b2d803f3602a1b8a5b7849f1517efd000a705c
FOnabircse <escribanofabricio1@gmail.com>
2026-08-18 15:36:29 -0300

    Add files via upload

---
+++ vectores/asistencias.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] asistencias = new string[20];
+            int faltas = 0;
+            Console.WriteLine("Ingrese (P) para poner presente o (A) para poner ausente.");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("Clase " + (i + 1) + ": ");
+                string entrada = Console.ReadLine();
+                while (entrada != "P" && entrada != "p" && entrada != "A" && entrada != "a")
+                {
+                    Console.Write("ERROR, Ingrese (P) o (A) : ");
+                    entrada = Console.ReadLine();
+                }
+
+                asistencias[i] = entrada;
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (asistencias[i] == "A" || asistencias[i] == "a")
+                {
+                    faltas++;
+                }
+            }
+            Console.WriteLine("Total de faltas: " + faltas);
+
+            if (faltas > 6)
+            {
+                Console.WriteLine("Estado del alumno: Libre por inasistencias");
+            }
+            else
+            {
+                Console.WriteLine("Estado del alumno: Regular");
+            }
+        }
+    }
+}
